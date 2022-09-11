6204dc378a05560efd51989e5281fce1b43493da
Anton Voropaiev <voropaiev.ios@gmail.com>
2022-09-11 11:16:17 -0400

    Base project setup

---
+++ HelloWorld/Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Anton Voropaiev on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
